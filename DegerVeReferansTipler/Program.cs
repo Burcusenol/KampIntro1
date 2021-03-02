b4f974881d044cca7f3424ec935e5497fa36639b
Burcu ŞENOL <burcusenol62@gmail.com>
2021-03-02 18:58:32 +0300

    Add project files.

---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+            //sayi1=30
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            //sayilar1[0]=999
+
+
+            //int,decimal,float,double,bool=değer tip--stack de tutulur
+            //array,class,interface=referans tip--heap de tutulur
+            //new:bellekte yeni bir yer tutmaya yarar
+            // REFERANS TİPLERDE ADRESE ATAMA YAPIYORSUN!!
+            //C dilinde pointer
+
+        }
+    }
+}
---
+++ Metotlar/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elması";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama="Diyarbakır Karpuzu";
+
+            //Ürün dizisi.Diziler çoğul tiğler içerir.
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+
+
+            //Ürünler dizisini tek tek gez ekrana yazdır
+            //urun:urun sayısı,dönen elemana verilen sayı
+            //Urun:Veri tipi
+
+            //type-safe--tip güvenli
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("*********************");
+            }
+
+            Console.WriteLine("------------------------Metotlar---------------");
+
+            //instance-örnek
+            //encapsulation
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil Armut", 12,10);
+            sepetManager.Ekle2("Elma", "Yeşil Elma", 12,9);
+            sepetManager.Ekle2("Karpuz", "Diyarbakır Karpuzu", 12,8);
+
+
+        }
+    }
+}
